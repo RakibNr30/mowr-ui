a688aa6cb57760392c0e22f875164b73a7629f8f
Abdur Rakib <rakib.cse14th@gmail.com>
2024-05-05 02:42:40 +0600

    design update for file

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 (function () {
   'use strict'
 
-  feather.replace({ 'aria-hidden': 'true' })
+  feather.replace({'aria-hidden': 'true'})
 
   const requiredElements = document.querySelectorAll('[required]');
 
@@ -11,3 +11,19 @@
     label.innerHTML += '<span style="color: red;"> *</span>';
   });
 })()
+
+/* Custom File Field */
+$(document).ready(function () {
+  $('.file-input-label').each(function () {
+    let defaultText = $(this).data('default-text');
+    let innerInput = $(this).find('input[type="file"]');
+    $(this).empty().text(defaultText).append(innerInput);
+  });
+
+  $(document).on('change', '.file-input', function (e) {
+    let fileName = (e.target.files.length > 0) ? e.target.files[0].name : $(this).parent('.file-input-label').data('default-text');
+    let targetId = $(this).attr('id');
+    let innerInput = $(this).parent('.file-input-label').find('input[type="file"]');
+    $('.file-input-label[data-target="' + targetId + '"]').empty().text(fileName).append(innerInput);
+  });
+});
